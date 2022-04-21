6858b037be0dfa7238c07228ac6615dad780cf93
saad <saadbinsaeed259@gmail.com>
2022-04-21 23:04:38 +0500

    Updates

---
+++ precache-manifest.4e61e12fa88a1facc20fd99d4bdc5574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3d7a04a0451d341c786ad477ce7903",
+    "url": "/saad/index.html"
+  },
+  {
+    "revision": "3bb3e7f2e49f54fd9dbd",
+    "url": "/saad/static/css/main.256727b6.chunk.css"
+  },
+  {
+    "revision": "d651f76f6e41539373b9",
+    "url": "/saad/static/js/2.7c37dedb.chunk.js"
+  },
+  {
+    "revision": "15f2a749186a67f27f167d76acad5499",
+    "url": "/saad/static/js/2.7c37dedb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb3e7f2e49f54fd9dbd",
+    "url": "/saad/static/js/main.754700b6.chunk.js"
+  },
+  {
+    "revision": "5bc50f3f9d6249d4e791",
+    "url": "/saad/static/js/runtime-main.2d01bacf.js"
+  }
+]);
